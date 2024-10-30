692787f1f853ad800264fde288bff8f2770d989d
lche0185 <lche0185@student.monash.edu>
2024-10-30 12:04:20 +1100

    implement bootstrap

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -15,45 +15,27 @@
  */
 
 $cakeDescription = 'CakePHP: the rapid development php framework';
-?>
-<?php
-// Check if the user is logged in
 $loggedIn = $this->request->getAttribute('identity') !== null;
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
     <title><?= $cakeDescription ?> - <?= $this->fetch('title') ?></title>
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap 4 Admin Theme CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <?= $this->Html->css(['sb-admin-2.min', 'sidebar', 'normalize.min', 'milligram.min', 'fonts', 'cake']) ?>
 
-    <!-- Custom fonts for this template -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-    <!-- FontAwesome icons -->
-
-    <!-- Custom CSS for SB Admin 2 and additional CSS files -->
-    <?= $this->Html->css(['normalize.min', 'milligram.min', 'fonts', 'cake']) ?>
-    <?= $this->Html->css(['all.min.css', 'sb-admin-2.min.css', 'sidebar.css']) ?>
+    <!-- FontAwesome for icons -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
-
-
-
-
 </head>
-
-
 <body id="page-top">
 
 <!-- Page Wrapper -->
@@ -61,110 +43,52 @@ $loggedIn = $this->request->getAttribute('identity') !== null;
 
     <!-- Sidebar -->
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
         <!-- Sidebar - Brand -->
-        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= $this->Url->build('/') ?>">
-            <div class="sidebar-brand-icon rotate-n-15">
-                <i class="fas fa-laugh-wink"></i>
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= $this->Url->build('/dashboard'); ?>">
+            <div class="sidebar-brand-icon">
+                <?= $this->Html->image('logo.png', ['alt' => 'Nathan\'s Business Logo', 'class' => 'logo', 'style' => 'height: 70px; width: auto;']) ?>
             </div>
-            <div class="sidebar-brand-text mx-3">Nathan Admin pages</div>
+            <div class="sidebar-brand-text mx-3">Admin</div>
         </a>
 
         <!-- Divider -->
         <hr class="sidebar-divider my-0">
 
-        <!-- Nav Items -->
+        <!-- Nav Item - Dashboard -->
         <li class="nav-item">
-            <a class="nav-link" href="<?= $this->Url->build('/dashboard') ?>">
+            <a class="nav-link" href="<?= $this->Url->build('/dashboard'); ?>">
                 <i class="fas fa-fw fa-tachometer-alt"></i>
                 <span>Dashboard</span>
             </a>
         </li>
 
-        <li class="nav-item">
-            <a class="nav-link" href="<?= $this->Url->build('/projects') ?>">
-                <i class="fas fa-fw fa-folder"></i>
-                <span>Projects</span>
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="<?= $this->Url->build('/contractors') ?>">
-                <i class="fas fa-fw fa-users"></i>
-                <span>Contractors</span>
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="<?= $this->Url->build('/organisations') ?>">
-                <i class="fas fa-fw fa-building"></i>
-                <span>Organisations</span>
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="<?= $this->Url->build('/contact') ?>">
-                <i class="fas fa-fw fa-envelope"></i>
-                <span>Contact Us</span>
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="<?= $this->Url->build('/skills') ?>">
-                <i class="fas fa-fw fa-tools"></i>
-                <span>Skills</span>
-            </a>
-        </li>
-
         <!-- Divider -->
-        <hr class="sidebar-divider d-none d-md-block">
+        <hr class="sidebar-divider">
 
-        <!-- Sidebar Toggler (Sidebar) -->
-        <div class="text-center d-none d-md-inline">
-            <button class="rounded-circle border-0" id="sidebarToggle"></button>
-        </div>
-
+        <!-- Conditional Links based on login status -->
+        <?php if ($loggedIn): ?>
+            <li class="nav-item"><a class="nav-link" href="<?= $this->Url->build('/projects'); ?>"><i class="fas fa-project-diagram"></i> Projects</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?= $this->Url->build('/contractors'); ?>"><i class="fas fa-users"></i> Contractors</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?= $this->Url->build('/organisations'); ?>"><i class="fas fa-building"></i> Organisations</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?= $this->Url->build('/contact'); ?>"><i class="fas fa-envelope"></i> Contact Us</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?= $this->Url->build('/skills'); ?>"><i class="fas fa-tools"></i> Skills</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?= $this->Url->build('/users/logout'); ?>"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+        <?php else: ?>
+            <li class="nav-item"><a class="nav-link" href="<?= $this->Url->build('/contractors/add'); ?>"><i class="fas fa-user-plus"></i> Register Contractor</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?= $this->Url->build('/organisations/add'); ?>"><i class="fas fa-plus-circle"></i> Register Organisation</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?= $this->Url->build('/contact/add'); ?>"><i class="fas fa-envelope"></i> Contact Us</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?= $this->Url->build('/users/login'); ?>"><i class="fas fa-sign-in-alt"></i> Login</a></li>
+        <?php endif; ?>
     </ul>
-    <!-- End of Sidebar -->
 
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
-
         <!-- Main Content -->
         <div id="content">
-
             <!-- Topbar -->
             <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-                <!-- Topbar Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <?php if ($loggedIn): ?>
-                        <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">
-                                        Welcome, <?= h($this->request->getAttribute('identity')->first_name) ?>
-                                    </span>
-                                <i class="fas fa-user-circle"></i>
-                            </a>
-                            <div class="nav-item dropdown no-arrow"
-                                 aria-labelledby="userDropdown">
-                                <?= $this->Html->link(
-                                    'Logout',
-                                    ['controller' => 'Users', 'action' => 'logout'],
-                                    ['class' => 'dropdown-item']
-                                ) ?>
-                            </div>
-                        </li>
-                    <?php else: ?>
-                        <li class="nav-item">
-                            <?= $this->Html->link('Login', ['controller' => 'Users', 'action' => 'login'], ['class' => 'nav-link']) ?>
-                        </li>
-                    <?php endif; ?>
-                </ul>
-
+                <h1 class="h3 mb-0 text-gray-800"><?= $this->fetch('title') ?></h1>
             </nav>
-            <!-- End of Topbar -->
 
             <!-- Begin Page Content -->
             <div class="container-fluid">
@@ -172,35 +96,16 @@ $loggedIn = $this->request->getAttribute('identity') !== null;
                 <?= $this->fetch('content') ?>
             </div>
             <!-- /.container-fluid -->
-
         </div>
         <!-- End of Main Content -->
-
-        <!-- Footer -->
-        <footer class="sticky-footer bg-white">
-            <div class="container my-auto">
-                <div class="copyright text-center my-auto">
-                    <span>Copyright &copy; My Application 2024</span>
-                </div>
-            </div>
-        </footer>
-        <!-- End of Footer -->
-
     </div>
     <!-- End of Content Wrapper -->
-
 </div>
 <!-- End of Page Wrapper -->
 
-<!-- Scroll to Top Button-->
-<a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-</a>
-
 <!-- Bootstrap core JavaScript-->
-<?= $this->Html->script(['vendor/jquery/jquery.min', 'vendor/bootstrap/js/bootstrap.bundle.min']) ?>
-<!-- Custom scripts for all pages-->
-<?= $this->Html->script('sb-admin-2.min') ?>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
 </body>
-
 </html>
+
