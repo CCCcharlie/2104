78e48bebbe9320773b6ded8f71df96e35e56bc1b
araj0056 <araj0056@student.monash.edu>
2024-10-30 16:17:45 +1100

    buttons added

--- templates/Contact/edit.php
+++ templates/Contact/edit.php
@@ -10,12 +10,19 @@
     <aside class="column">
         <div class="side-nav">
             <h4 class="heading"><?= __('Actions') ?></h4>
+            <!-- Back to contact List -->
+            <?= $this->Html->link(__('Back'), ['action' => 'index'], ['class' => 'button', 'style' => 'color: white;']) ?>
+
+            <!-- Delete contact -->
             <?= $this->Form->postLink(
                 __('Delete'),
                 ['action' => 'delete', $contact->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $contact->id), 'class' => 'side-nav-item']
+                [
+                    'confirm' => __('Are you sure you want to delete # {0}?', $contact->id),
+                    'class' => 'button',
+                    'style' => 'color: white; background-color: #dc3545;'
+                ]
             ) ?>
-            <?= $this->Html->link(__('List Contact'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
     <div class="column column-80">
