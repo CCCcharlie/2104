bd45c3c41e0a2b875a3e5d68b63ccbbbb15439a2
lche0185 <lche0185@student.monash.edu>
2024-11-01 23:28:21 +1100

    updated the input field restriction

--- templates/Projects/add.php
+++ templates/Projects/add.php
@@ -23,29 +23,50 @@
             <fieldset>
                 <legend><?= __('Add Project') ?></legend>
                 <?php
-                    echo $this->Form->control('project_name');
-                    echo $this->Form->control('description');
-                    echo $this->Form->control('management_tool_link');
-                    echo $this->Form->control('project_due_date', ['empty' => true]);
-                    echo $this->Form->control('last_checked', ['empty' => true]);
-                    echo $this->Form->control('complete');
-                    echo $this->Form->control('contractor_id', [
-                        'options' => $contractors,
-                        'empty' => 'Select Contractor',
-                        'label' => 'Contractor Name'
-                    ]);
+                echo $this->Form->control('project_name', [
+                    'required' => true,
+                    'label' => 'Project Name'
+                ]);
+                echo $this->Form->control('description', [
+                    'required' => true,
+                    'label' => 'Description'
+                ]);
+                echo $this->Form->control('management_tool_link', [
+                    'required' => true,
+                    'label' => 'Management Tool Link',
+                    'type' => 'url', // Set the type to url to validate URL format
+                    'placeholder' => 'https://example.com' // Optional: provide a placeholder for clarity
+                ]);
+                echo $this->Form->control('project_due_date', [
+                    'empty' => true,
+                    'required' => true,
+                    'label' => 'Due Date'
+                ]);
+                echo $this->Form->control('last_checked', [
+                    'empty' => true,
+                    'label' => 'Last Checked'
+                ]);
+                echo $this->Form->control('complete', [
+                    'label' => 'Complete'
+                ]);
+                echo $this->Form->control('contractor_id', [
+                    'options' => $contractors,
+                    'empty' => 'Select Contractor',
+                    'label' => 'Contractor Name'
+                ]);
 
-                    echo $this->Form->control('organisation_id', [
-                        'options' => $organisations,
-                        'empty' => 'Select Organisation',
-                        'label' => 'Organisation Name'
-                    ]);
-                    echo $this->Form->control('skills._ids', [
-                        'type' => 'select',
-                        'multiple' => 'checkbox',
-                        'options' => $skills,
-                        'label' => 'Select Skills'
-                    ]);
+                echo $this->Form->control('organisation_id', [
+                    'options' => $organisations,
+                    'empty' => 'Select Organisation',
+                    'required' => true,
+                    'label' => 'Organisation Name'
+                ]);
+                echo $this->Form->control('skills._ids', [
+                    'type' => 'select',
+                    'multiple' => 'checkbox',
+                    'options' => $skills,
+                    'label' => 'Select Skills',
+                ]);
                 ?>
             </fieldset>
             <?= $this->Form->button(__('Submit')) ?>
